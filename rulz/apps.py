cbe64bbc3192cc06d2444596e3d17c30513729b1
MBoidot <mathieu.boidot@gmail.com>
2022-01-09 21:54:27 +0100

    removing rulz and shop apps

--- rulz/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RulzConfig(AppConfig):
-    name = 'rulz'
--- shop/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def shop_index(request):
-    template_name = "shop/home_template.html"
-    return render(request,template_name)
