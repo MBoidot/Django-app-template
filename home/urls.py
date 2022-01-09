08fba32bf975f2faec191cd0042a4d20c85146b7
MBoidot <mathieu.boidot@gmail.com>
2022-01-09 22:41:07 +0100

    logout implementation

--- home/urls.py
+++ home/urls.py
@@ -2,15 +2,14 @@ from django.urls import re_path
 from django.contrib import admin
 
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 app_name = 'home'
 
 urlpatterns = [
     re_path(r'^login/$', LoginView.as_view(template_name='home/login.html'), name='login'),
-
-    re_path(r'^logout/$', LoginView.as_view(template_name='home/login.html'),name='logout'),
+    re_path(r'^logout/$', LogoutView.as_view(), name='logout'),
     re_path(r'^register/$', views.UserFormView.as_view(), name='register'),
     re_path(r'^', views.HomeView, name='home'),
 ]
